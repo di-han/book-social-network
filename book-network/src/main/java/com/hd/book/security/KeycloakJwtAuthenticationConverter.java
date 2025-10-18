dfe585eec84e765fdd799a1cbf245a2d622d1976
handi <3283606583@qq.com>
2025-10-18 15:58:53 +0800

    提取令牌中realm_access项中的 roles 列表

--- book-network/src/main/java/com/hd/book/security/KeycloakJwtAuthenticationConverter.java
+++ book-network/src/main/java/com/hd/book/security/KeycloakJwtAuthenticationConverter.java
@@ -28,10 +28,12 @@ public class KeycloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
     // JWT 令牌中提取这个 resource_access 对象，然后提取 account 对象，
     // 然后提取 roles 列表，最后将每个 roles 中的每个 role 转换为 spring role
     public Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        var resourceAccess = new HashMap<>(jwt.getClaim("resource_access"));
-        var eternal = (Map<String, List<String>>) resourceAccess.get("account");
-        var roles = eternal.get("roles");
-
+        // var resourceAccess = new HashMap<>(jwt.getClaim("resource_access"));
+        // var eternal = (Map<String, List<String>>) resourceAccess.get("account");
+        var realmAccess = new HashMap<>(jwt.getClaim("realm_access"));
+        System.out.println(realmAccess);
+        var roles = (List<String>) realmAccess.get("roles");
+        System.out.println(roles);
         return roles.stream()
                 //  spring 角色默认是 `ROLE_[角色名称]`，并且若角色名称中有 `-`，需替换为 `_`
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.replace("-", "_")))

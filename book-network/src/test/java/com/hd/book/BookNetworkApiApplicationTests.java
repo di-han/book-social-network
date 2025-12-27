6f5b1804281aa89e8cbbb4819b48b3fd766a5727
handi <3283606583@qq.com>
2025-12-27 23:38:47 +0800

    remove @SpringBootTest

--- book-network/src/test/java/com/hd/book/BookNetworkApiApplicationTests.java
+++ book-network/src/test/java/com/hd/book/BookNetworkApiApplicationTests.java
@@ -3,7 +3,6 @@ package com.hd.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BookNetworkApiApplicationTests {
 
 	@Test
